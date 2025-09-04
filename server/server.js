d4c63f6c8cd850c279be8bd5bd2b68b7efb4db97
Shalombay7 <shalombay7@gmail.com>
2025-09-03 22:47:03 -0700

    Update server.js to serve client build

--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
+const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/products');
 const cartRoutes = require('./routes/cart');
-const authRoutes = require('./routes/auth');
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-
+app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
-app.use('/api/auth', authRoutes);
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(process.env.MONGO_URI)
+// Serve React build
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
